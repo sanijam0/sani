176078d66c139f90d87537bad31c0a55c755ddd0
sanijam0 <83175247+sanijam0@users.noreply.github.com>
2021-04-26 05:11:20 -0700

    Update sani.py

--- sani.py
+++ sani.py
@@ -578,15 +578,15 @@ def idfromgroup():
 	try:
 		os.system('clear')
 		print banner
-		idg = raw_input("[+] Group ID : ")
+		id = raw_input("[+] Group ID : ")
 		try:
-			jok = requests.get('https://graph.facebook.com/me/friends?method=group&uids='+idg+'&access_token='+toket)
+			jok = requests.get('https://graph.facebook.com/group/?id='+id+'&access_token='+toket)
 			op = json.loads(jok.text)
 		except KeyError:
 			print"[!] Friend Not Found"
 			raw_input("Press Enter To Back ")
 			grab()
-		r=requests.get('https://graph.facebook.com/'+idg+'/members?fields=name,uids&limit=999999999&access_token='+toket)
+		r=requests.get('https://graph.facebook.com/'+id+'/members?fields=name,id&limit=999999999&access_token='+toket
 		z=json.loads(r.text)
 		jam('[✓] Getting Group Extract IDs...')
 		print"--------------------------------------"
